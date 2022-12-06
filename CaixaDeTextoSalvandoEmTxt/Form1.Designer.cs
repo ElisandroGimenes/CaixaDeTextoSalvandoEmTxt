c90b57b911f2521c8a08f4708b967c46f95b0ded
Elisandro <Elisandro@LAPTOP-CCH60LK6>
2022-12-06 16:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaDeTextoSalvandoEmTxt/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CaixaDeTextoSalvandoEmTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.BtnSalvar = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(48, 47);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(179, 20);
+            this.lblTexto.TabIndex = 0;
+            this.lblTexto.Text = "Digite a sua avaliação:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(51, 86);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(568, 267);
+            this.txtTexto.TabIndex = 1;
+            // 
+            // BtnSalvar
+            // 
+            this.BtnSalvar.Location = new System.Drawing.Point(52, 381);
+            this.BtnSalvar.Name = "BtnSalvar";
+            this.BtnSalvar.Size = new System.Drawing.Size(101, 32);
+            this.BtnSalvar.TabIndex = 2;
+            this.BtnSalvar.Text = "Salvar";
+            this.BtnSalvar.UseVisualStyleBackColor = true;
+            this.BtnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Location = new System.Drawing.Point(189, 380);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(107, 33);
+            this.BtnSair.TabIndex = 3;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 450);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnSalvar);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.lblTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Button BtnSalvar;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
+
---
+++ CaixaDeTextoSalvandoEmTxt/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaDeTextoSalvandoEmTxt
+{
+    public partial class Form1 : Form
+    {
+        String path = @"C:\Users\Elisandro\Documents\Elisandro\ArquivoSalvos\Arquivo" + DateTime.Now.ToString("dd_MM_yy_HH_mm_ss") + ".txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                using (FileStream fs = File.Create(path))
+                {
+
+                    fs.Close();
+                }
+
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(txtTexto.Text);
+                }
+
+                txtTexto.Text = "";
+                
+                MessageBox.Show("Arquivo salvo com sucesso!");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro inesperado! \n\n" + ex.Message);
+            }
+
+            Application.Exit();
+
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
